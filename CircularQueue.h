55edef560637984a9e9b9ece3723ec2f8abc5abb
Amir <amir.noroozi3319@gmail.com>
2021-05-15 21:32:09 +0430

    add methods and fields to CircularQueue.h

--- CircularQueue.h
+++ CircularQueue.h
@@ -1,9 +1,26 @@
 #ifndef MIDTERMPROJECT_CIRCULARQUEUE_H
 #define MIDTERMPROJECT_CIRCULARQUEUE_H
-
+#include <any>
 
 class CircularQueue {
 
+private:
+
+    int rear, front;
+    int size;
+    std::any* elements;
+
+public:
+
+    CircularQueue(int s);
+    CircularQueue();
+
+
+    void enQueue(int value);
+    std::any deQueue();
+    bool isEmpty();
+    bool isFull();
+    int getNum();
 
 
 };
