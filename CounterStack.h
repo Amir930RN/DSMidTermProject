e138fb2b093dd16a1bfe716284d27cc8633bdbb7
Amir <amir.noroozi3319@gmail.com>
2021-05-16 16:23:17 +0430

    add methods and fields to CounterStack.h

--- CounterStack.h
+++ CounterStack.h
@@ -1,8 +1,27 @@
 #ifndef MIDTERMPROJECT_COUNTERSTACK_H
 #define MIDTERMPROJECT_COUNTERSTACK_H
 
+#include <any>
 
 class CounterStack {
+
+private:
+
+    int top;
+    std::any * elements;
+    int size;
+
+public:
+
+    CounterStack ( int s);
+    CounterStack ( );
+    ~CounterStack ( );
+
+    void push(const std::any& value);
+    std::any pop();
+    bool isEmpty();
+    bool isFull();
+    int getNum();
 
 };
 
